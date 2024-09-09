86e356a1b27ce06fe641aedef4089af850f0eebf
oligamiq <nziq53@gmail.com>
2024-09-10 04:58:59 +0900

    save

--- src/wasi_farm.ts
+++ src/wasi_farm.ts
@@ -28,30 +28,9 @@ export default class WASIFarm {
   }
 
   member_ref(): [Array<string>, Array<string>, Array<Fd>] {
-    const args = new Proxy([] as Array<string>, {
-      get: (_, prop) => {
-        if (prop === "push") {
-          return (fd: Fd) => {
-            this.fds.push(fd);
-          };
-        }
+    const args = [...this.args];
+    const env = [...this.env];
 
-        console.log("args", prop);
-        return this.args[prop];
-      }
-    });
-    const env = new Proxy([] as Array<string>, {
-      get: (_, prop) => {
-        if (prop === "push") {
-          return (fd: Fd) => {
-            this.fds.push(fd);
-          };
-        }
-
-        console.log("env", prop);
-        return this.env[prop];
-      }
-    });
     const fds = new Proxy([] as Array<Fd>, {
       get: (_, prop) => {
         console.log("fds", prop);
@@ -68,18 +47,13 @@ export default class WASIFarm {
     return [args, env, fds];
   }
 
-  get_ref(): [WASIFarmRef, (socket: Channel) => void] {
+  get_ref(): WASIFarmRef {
     let socket = new WASIFarmPark(this.member_ref());
 
-    return [new WASIFarmRef(
-      socket.socket_(),
-    ), socket.register];
-  }
-}
+    this.sockets.push(socket);
 
-type U8 = number;
-type Message = {
-  func_id: U8;
+    return socket.get_ref();
+  }
 }
 
 export class WASIFarmPark {
@@ -107,6 +81,7 @@ export class WASIFarmPark {
   // 解放するときは、Atomics.subで1減らすだけ。
 
   // args, envは変更されないので、コピーで良い
+  // fdsに依存しない関数は飛ばす
   // wasm32なので、pointerはu32
   // errnoはu8
   // https://github.com/WebAssembly/WASI/blob/4feaf733e946c375b610cc5d39ea2e1a68046e62/legacy/preview1/docs.md
@@ -142,54 +117,73 @@ export class WASIFarmPark {
   //    use share_arrays_memory;
   //    (fd: u32, buf_len: u32, cookie: u64) => [buf_ptr: pointer, buf_len: u32, errno];
   // fd_renumber: (fd: u32, to: u32) => errno;
-  access_fds: SharedArrayBuffer = new SharedArrayBuffer(9);
+  // fd_seek: (fd: u32, offset: i64, whence: u8) => [u64, errno];
+  // fd_sync: (fd: u32) => errno;
+  // fd_tell: (fd: u32) => [u64, errno];
+  // fd_write: (fd: u32, iovs_ptr: pointer, iovs_len: u32) => [u32, errno];
+  //    use share_arrays_memory;
+  //    (fd: u32, write_data: pointer, write_data_len: u32) => [u32, errno];
+  // path_create_directory: (fd: u32, path_ptr: pointer, path_len: u32) => errno;
+  // path_filestat_get: (fd: u32, flags: u32, path_ptr: pointer, path_len: u32) => [wasi.Filestat(u32 * 16), errno];
+  // path_filestat_set_times: (fd: u32, flags: u32, path_ptr: pointer, path_len: u32, atim: u64, mtim: u64, fst_flags: u16) => errno;
+  // path_link: (old_fd: u32, old_flags: u32, old_path_ptr: pointer, old_path_len: u32, new_fd: u32, new_path_ptr: pointer, new_path_len: u32) => errno;
+  // path_open: (fd: u32, dirflags: u32, path_ptr: pointer, path_len: u32, oflags: u32, fs_rights_base: u64, fs_rights_inheriting: u64, fs_flags: u16, fdflags: u16) => [u32, errno];
+  // note: fdsにpushするが、既存のfdに影響しないので、競合しない。
+  // path_readlink: (fd: u32, path_ptr: pointer, path_len: u32, buf_ptr: pointer, buf_len: u32) => [u32, errno];
+  //    use share_arrays_memory;
+  //    (fd: u32, path_ptr: pointer, path_len: u32, buf_len: u32) => [buf_ptr: pointer, size: u32, errno];
+  // path_remove_directory: (fd: u32, path_ptr: pointer, path_len: u32) => errno;
+  // path_rename: (old_fd: u32, old_path_ptr: pointer, old_path_len: u32, new_fd: u32, new_path_ptr: pointer, new_path_len: u32) => errno;
+  // path_symlink: (old_path_ptr: pointer, old_path_len: u32, fd: u32, new_path_ptr: pointer, new_path_len: u32) => errno;
+  // path_unlink_file: (fd: u32, path_ptr: pointer, path_len: u32) => errno;
+
+  // ここから、fdを使わないもの
+  // 上と競合せずに使える。
+  // poll_oneoff: (in_ptr: pointer, out_ptr: pointer, nsubscriptions: u32, nevents_ptr: pointer) => errno;
+  // note: async io not supported
+  // proc_exit: (rval: u32) => never;
+  // proc_raise: (sig: u8) => errno;
+  // sched_yield: () => errno;
+  // random_get: (buf_ptr: pointer, buf_len: u32) => errno;
+  // sock_recv: "sockets not supported";
+  // sock_send: "sockets not supported";
+  // sock_shutdown: "sockets not supported";
+  // sock_accept: "sockets not supported";
+
+  // fdを使いたい際に、ロックする
+  lock_fds: SharedArrayBuffer = new SharedArrayBuffer(9);
+  // 一番大きなサイズはu32 * 16 + 1
+  // Alignが面倒なので、u32 * 16 + 4にする
+  fd_func_sig: SharedArrayBuffer = new SharedArrayBuffer(68);
+
+  constructor([args, env, fds]: [Array<string>, Array<string>, Array<Fd>]) {
+    this.args = args;
+    this.env = env;
+    this.fds = fds;
+  }
+
+  /// これをpostMessageで送る
+  get_ref(): WASIFarmRef {
+    return new WASIFarmRef(
+      this.share_arrays_memory,
+      this.lock_fds,
+      this.fd_func_sig,
+    );
+  }
 }
 
 export class WASIFarmRef {
-  socket: SharedArrayBuffer[];
+  share_arrays_memory: SharedArrayBuffer;
+  lock_fds: SharedArrayBuffer;
+  fd_func_sig: SharedArrayBuffer;
 
-  constructor(socket: SharedArrayBuffer[]) {
-    this.socket = socket;
-  }
-
-  get_wasi(): WASI {
-    const args = new Proxy([] as Array<string>, {
-      get: (_, prop) => {
-        if (prop === "push") {
-          return (fd: Fd) => {
-            this.fds.push(fd);
-          };
-        }
-
-        console.log("args", prop);
-        return this.args[prop];
-      }
-    });
-    const env = new Proxy([] as Array<string>, {
-      get: (_, prop) => {
-        if (prop === "push") {
-          return (fd: Fd) => {
-            this.fds.push(fd);
-          };
-        }
-
-        console.log("env", prop);
-        return this.env[prop];
-      }
-    });
-    const fds = new Proxy([] as Array<Fd>, {
-      get: (_, prop) => {
-        console.log("fds", prop);
-
-        if (prop === "push") {
-          return (fd: Fd) => {
-            this.fds.push(fd);
-          };
-        }
-        return this.fds[prop];
-      }
-    });
-
-    return new WASI(args, env, fds);
+  constructor(
+    share_arrays_memory: SharedArrayBuffer,
+    lock_fds: SharedArrayBuffer,
+    fd_func_sig: SharedArrayBuffer,
+  ) {
+    this.share_arrays_memory = share_arrays_memory;
+    this.lock_fds = lock_fds;
+    this.fd_func_sig = fd_func_sig;
   }
 }
