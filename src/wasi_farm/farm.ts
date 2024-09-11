2e043e636284f23e6952b1872c0c57c01c56e188
oligamiq <nziq53@gmail.com>
2024-09-11 21:50:37 +0900

    save

--- src/wasi_farm/farm.ts
+++ src/wasi_farm/farm.ts
@@ -6,23 +6,17 @@ import { WASIFarmRef } from "./ref.js";
 import { WASIFarmParkUseArrayBuffer } from "./shared_array_buffer/park.js";
 
 export default class WASIFarm {
-  private args: Array<string>;
-  private env: Array<string>;
   private fds: Array<Fd>;
   private park: WASIFarmPark;
 
   private can_array_buffer;
 
   constructor(
-    args: Array<string>,
-    env: Array<string>,
     fds: Array<Fd>,
     options: Options = {},
   ) {
     debug.enable(options.debug);
 
-    this.args = args;
-    this.env = env;
     this.fds = fds;
 
     try {
