840b3e30c155b07789f90c89e0cd1b85b6711215
oligamiq <nziq53@gmail.com>
2024-09-11 21:48:43 +0900

    save

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import WASI, { WASIProcExit } from "./wasi.js";
 export { WASI, WASIProcExit };
 import WASIFarm from "./wasi_farm/farm.js";
 export { WASIFarm };
-import { WASIFarmRef } from "./wasi_farm/ref/ref.js";
+import { WASIFarmRef } from "./wasi_farm/ref.js";
 export { WASIFarmRef };
 import { WASIFarmPark } from "./wasi_farm/park.js";
 export { WASIFarmPark };
